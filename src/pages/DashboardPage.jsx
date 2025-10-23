b5c332538c18d71ec1e0b450cccc01182a364763
firhat <mohd.firhat@hotmail.com>
2025-10-23 11:14:21 +0800

    creating folder template

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,11 @@
+import FirstComponent from "../components/FirstComponent";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>DashboardPage</h1>
+      <FirstComponent />
+    </div>
+  );
+};
+export default DashboardPage;
