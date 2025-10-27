73fb965601ab365fe1460dc5efa736680f799e13
firhat <mohd.firhat@hotmail.com>
2025-10-27 13:54:45 +0800

    added more events on calender and added diff colors for diff teachers

--- src/fakedata/data.js
+++ src/fakedata/data.js
@@ -22,12 +22,36 @@ export const lessons = [
     class_size: 10,
     status: "confirmed",
   },
+  {
+    teacher: {
+      id: 3,
+      first_name: "Grace",
+      last_name: "Hopper",
+      email: "grace.hopper@gmail.com",
+    },
+    venue: {
+      name: "LT-101",
+      address: "Address 1",
+      description: "Lecture Hall 1",
+      occupancy: 10,
+    },
+    subject: {
+      id: "CS101",
+      name: "Programming Fundamentals",
+    },
+    description: "",
+    start_time: "2025-10-27 08:00:00",
+    end_time: "2025-10-27 10:00:00",
+    class_size: 10,
+    status: "confirmed",
+  },
 ];
 
 export const leaves = [
   {
     id: 1,
     teacher: {
+      id: 2,
       first_name: "Alan",
       last_name: "Turing",
       email: "alan.turing@gmail.com",
@@ -36,4 +60,18 @@ export const leaves = [
     end_date: "2025-10-28",
     status: "pending",
   },
+  {
+    id: 2,
+    teacher: {
+      id: 3,
+      first_name: "Grace",
+      last_name: "Hopper",
+      email: "grace.hopper@gmail.com",
+    },
+    start_date: "2025-10-27",
+    end_date: "2025-10-28",
+    status: "pending",
+  },
 ];
+const today = new Date();
+console.log(`${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`);
